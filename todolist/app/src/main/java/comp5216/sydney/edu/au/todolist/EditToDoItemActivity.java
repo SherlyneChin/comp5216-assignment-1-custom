692606a1ff423d29a338fe4c635ddc56573de527
Shiah Jing Chin <schi2606@uni.sydney.edu.au>
2019-09-01 00:56:45 +1000

    add addNewItem function

--- todolist/app/src/main/java/comp5216/sydney/edu/au/todolist/EditToDoItemActivity.java
+++ todolist/app/src/main/java/comp5216/sydney/edu/au/todolist/EditToDoItemActivity.java
@@ -1,19 +1,28 @@
 package comp5216.sydney.edu.au.todolist;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class EditToDoItemActivity extends Activity
 {
 	public int position=0;
 	EditText etItem;
-
+	ArrayList<listItem> arrayOfItems;
+	itemAdapter itemAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,7 +31,8 @@ public class EditToDoItemActivity extends Activity
 		setContentView(R.layout.activity_edit_item);
 		
 		//Get the data from the main screen
-		String editItem = getIntent().getStringExtra("item");
+		String editItem = getIntent().getStringExtra("title");
+
 		position = getIntent().getIntExtra("position",-1);
 		
 		// show original content in the text field
@@ -31,13 +41,17 @@ public class EditToDoItemActivity extends Activity
 	}
 
 	public void onSubmit(View v) {
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		String dateStr = dateFormat.format(date);
 		etItem = (EditText) findViewById(R.id.etEditItem);
 
 		// Prepare data intent for sending it back
 		Intent data = new Intent();
 
 		// Pass relevant data back as a result
-		data.putExtra("item", etItem.getText().toString());
+		data.putExtra("title", etItem.getText().toString());
+		data.putExtra("dateTime", dateStr );
 		data.putExtra("position", position);
 
 		// Activity finished ok, return the data
@@ -46,6 +60,23 @@ public class EditToDoItemActivity extends Activity
 	}
 
 	public void onCancel(View v) {
-
+		Log.i("EditToDoItemActivity","Cancel Clicked ");
+		AlertDialog.Builder builder = new AlertDialog.Builder(EditToDoItemActivity.this);
+		builder.setTitle(R.string.dialog_cancel_title)
+				.setMessage(R.string.dialog_cancel_msg)
+				.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						Intent returnToMainActivityPage = new Intent(getApplicationContext(), MainActivity.class);
+						startActivity(returnToMainActivityPage);
+					}
+				})
+				.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						//
+					}
+				});
+		builder.create().show();
 	}
 }
--- todolist/app/src/main/java/comp5216/sydney/edu/au/todolist/MainActivity.java
+++ todolist/app/src/main/java/comp5216/sydney/edu/au/todolist/MainActivity.java
@@ -4,19 +4,27 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
+
 public class MainActivity extends AppCompatActivity {
+    private final int ADD_ITEM_REQUEST_CODE = 646;
+    private final int EDIT_ITEM_REQUEST_CODE = 647;
     ListView listView;
     ArrayList<listItem> arrayOfItems;
     itemAdapter itemAdapter;
+    listItem listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         //listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
         setupListViewListener();
     }
+
+
 
     private void setupListViewListener(){
         ListView listView = (ListView) findViewById(R.id.lstView);
@@ -66,8 +76,63 @@ public class MainActivity extends AppCompatActivity {
                 listItem updateItem = itemAdapter.getItem(position);
                 Log.i("MainActivity", "Clicked item"+position+": ");
 
+                Intent intent = new Intent(MainActivity.this, EditToDoItemActivity.class);
+                if (intent != null){
+                    intent.putExtra("title", updateItem.getTitle());
+                    intent.putExtra("dateTime",updateItem.getDateTime());
+                    intent.putExtra("position", position);
+
+                    startActivityForResult(intent, EDIT_ITEM_REQUEST_CODE);
+                    itemAdapter.notifyDataSetChanged();
+                }
             }
         });
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == EDIT_ITEM_REQUEST_CODE){
+            if (resultCode == RESULT_OK) {
+                String editedItem = data.getExtras().getString("title");
+                int position = data.getIntExtra("position", -1);
+                String dateTime = data.getExtras().getString("dateTime");
+                listItem = new listItem(editedItem,dateTime);
+                arrayOfItems.set(position, listItem);
+                Log.i("Updated Item in list: ", editedItem + ",position: "+position);
+
+                Toast.makeText(this, "updated: "+ editedItem, Toast.LENGTH_SHORT).show();
+                itemAdapter.notifyDataSetChanged();
+            }
+        } else if (requestCode == ADD_ITEM_REQUEST_CODE){
+            if (resultCode == RESULT_OK) {
+                String editedItem = data.getExtras().getString("title");
+                int position = data.getIntExtra("position", -1);
+                String dateTime = data.getExtras().getString("dateTime");
+                listItem = new listItem(editedItem,dateTime);
+                arrayOfItems.add(listItem);
+                Log.i("Added Item in list: ", editedItem);
+
+                Toast.makeText(this, "added: "+ editedItem, Toast.LENGTH_SHORT).show();
+                itemAdapter.notifyDataSetChanged();
+            }
+        }
+
+
+    }
+
+    public void onAddItemClick(View view) {
+        //Intent intent = new Intent(MainActivity.this, EditToDoItemActivity.class);
+        //Intent goAddEditPage = new Intent(getApplicationContext(), EditToDoItemActivity.class);
+        //startActivity(goAddEditPage);
+        Intent goAddEditPage = new Intent(MainActivity.this, EditToDoItemActivity.class);
+        startActivityForResult(goAddEditPage, ADD_ITEM_REQUEST_CODE);
+        itemAdapter.notifyDataSetChanged();
+    }
+
+//    private void readItemsFromFile(){
+//        File filesDir = getFilesDir();
+//
+//        File todoFile = new File(filesDir, "todo.txt");
+//
+//        if (!todoFile)
+//    }
 }
